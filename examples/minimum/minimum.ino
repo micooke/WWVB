0f2523f3df23cd280885ad6afd97e43cb7c41771
Mark Cooke <mark@creativecookies.net>
2016-01-18 22:54:06 +1030

    ## minimum.ino * removed ASCII ATtiny85 pinout, but replaced it with a table of the correct pinout for all supported chips (same as whats in wwvb.h) * removed the counter - i now output the debug data at the end of a frame (once it has rolled over into the new frame). I also moved the LED pulse to this same location - so theres a 100ms pulse at the end of each 60s frame. * Streamlined the #if defs - now all 3 chips use the same interrupt : ISR(TIMER1_OVF_vect) * TODO: calibrate the ATmega328p - im still out by 83ms i.e. a 60s frame is transmitted in 50.917s ## wwvb.h * added pinout info to header comment * reformatted raw() output so that its table output is in-line * added end_of_frame public variable. Its a sticky bit (doesnt get auto-reset), its intention is clear by looking at minimum.ino - but basically it is so you can determine the end of frame in the main arduino loop() * ATtinyX5 was converted to use the base synchronous clock as opposed to the asynchronous PLL (which is for 16MHz only) * added WWVB_EOB_CAL - a calibration value to be added at the end of each 1s bit. I found that the full frame took 83ms less than the full 60s to complete. Im still working on this, but the calibration value should be close to {1,2} as 1.5*60 = 80ms - but {1,2} didnt actually anything! ## wwvb_debugging.txt * further debugging added

--- examples/minimum/minimum.ino
+++ examples/minimum/minimum.ino
@@ -1,17 +1,10 @@
 /*
-ATtiny85
-
-                       +-\/-+
-                  RST 1|*   |8 VCC
-                  PB3 2|    |7 PB2
- WWVB ANTENNA =>| PB4 3|    |6 PB1
-                  GND 4|    |5 PB0
-                       +----+
-OC1A : PB1
-OC1B : PB4
+Minimum.ino
+Set the wwvb time to the compile time.
+The wwvb interrupt routine auto-increments the minute at the end of each frame 
 
 Recommended debug setup
-Use a RC (lowpass) filter to view the message (p1) as well as the modulated carrier (p0)
+Use a RC (low-pass) filter to view the message (p1) as well as the modulated carrier (p0)
 
 T = RC = 1/(2*pi*fc)
 where;
@@ -19,25 +12,37 @@ T: Time constant
 fc: cutoff frequency
 
 for: R = 110 ohm, C = 1uF
-fc = 1/(2*pi*RC) = 1.45kHz (the carrier is 60kHz, the modulation is about 1 to 5 Hz) 
+fc = 1/(2*pi*RC) = 1.45kHz (the carrier is 60kHz, the modulation is about 1 to 5 Hz)
 
-PB4 |--(p0)--[110R]--(p1)--[1uF]--|GND
+WWVB_OUT |--(p0)--[110R]--(p1)--[1uF]--|GND
+
+-----------+-----------+-----------------
+Chip       | #define   | WWVB_OUT
+-----------+-----------+-----------------
+ATtiny85   |  USE_OC1A | D1 / PB1 (pin 6)
+ATtiny85   | *USE_OC1B | D4 / PB4 (pin 3)
+-----------+-----------+-----------------
+ATmega32u4 | *USE_OC1A | D9
+ATmega32u4 |  USE_OC1B | D10
+-----------+-----------+-----------------
+ATmega328p | *USE_OC1A | D9
+ATmega328p |  USE_OC1B | D10
+-----------+-----------+-----------------
 */
+
 #include <Arduino.h>
 #include <avr/interrupt.h>
 
 #if defined(__AVR_ATtiny25__) | defined(__AVR_ATtiny45__) | defined(__AVR_ATtiny85__)
-#define USE_OC1B
 #define _DEBUG 0
 #else
-#define USE_OC1A
 #define _DEBUG 2
 #endif
 
 /*
- _DEBUG == 0: Set wwvb time to the compile time, use the wwvb interrupt, dont blink the led
- _DEBUG == 1: Set wwvb time to the compile time, use the wwvb interrupt, blink the led
- _DEBUG == 2: Set wwvb time to the compile time, use the wwvb interrupt, blink the led, serial output
+_DEBUG == 0: Set wwvb time to the compile time, use the wwvb interrupt, dont blink the led
+_DEBUG == 1: Set wwvb time to the compile time, use the wwvb interrupt, blink the led
+_DEBUG == 2: Set wwvb time to the compile time, use the wwvb interrupt, blink the led, serial output
 */
 #if (_DEBUG > 0)
 #if defined(__AVR_ATtiny25__) | defined(__AVR_ATtiny45__) | defined(__AVR_ATtiny85__)
@@ -56,72 +61,59 @@ boolean LED_TOGGLE = false;
 #include <wwvb.h> // include before ATtinyGPS.h
 wwvb wwvb_tx;
 
-struct counter
-{
-  uint8_t loop : 3;
-  uint8_t interrupt;
-};
-
-volatile counter count;
-
 // The ISR sets the PWM pulse width to correspond with the WWVB bit
-#if defined(__AVR_ATtiny25__) | defined(__AVR_ATtiny45__) | defined(__AVR_ATtiny85__)
-#if defined(USE_OC1A)
-ISR(TIMER1_COMPA_vect)
-#else
-ISR(TIMER1_COMPB_vect)
-#endif
-#else
 ISR(TIMER1_OVF_vect)
-#endif
 {
-	cli(); // disable interrupts
-	
-	wwvb_tx.interrupt_routine();
-	
-	sei(); // enable interrupts
+   cli(); // disable interrupts
+   
+   wwvb_tx.interrupt_routine();
+   
+   sei(); // enable interrupts
 }
 
 void setup()
 {
-	wwvb_tx.setup();
+   wwvb_tx.setup();
 
-  #if (REQUIRE_TIMEDATESTRING == 1)
-	wwvb_tx.set_time(__DATE__, __TIME__);
-  #endif
-  
-  #if (_DEBUG == 2)
-  Serial.begin(9600);
-  while(!Serial);
-    
-  Serial.print("wwvb set to: ");
-  Serial.print(__TIME__);
-  Serial.print(" on ");
-  Serial.println(__DATE__);
-  #endif
-  
-  #if (_DEBUG > 0)
-	pinMode(LED_PIN, OUTPUT);
-	digitalWrite(LED_PIN, HIGH);
-  #endif
+   #if (REQUIRE_TIMEDATESTRING == 1)
+   wwvb_tx.set_time(__DATE__, __TIME__);
+   #endif
+   
+   #if (_DEBUG == 2)
+   Serial.begin(9600);
+   while(!Serial);
+   
+   Serial.print("wwvb set to: ");
+   Serial.print(__TIME__);
+   Serial.print(" on ");
+   Serial.println(__DATE__);
+   
+   wwvb_tx.debug_time();
+   Serial.println();
+   #endif
+   
+   #if (_DEBUG > 0)
+   pinMode(LED_PIN, OUTPUT);
+   digitalWrite(LED_PIN, HIGH);
+   #endif
 
-  wwvb_tx.start(); // Thats it
+   wwvb_tx.start(); // Thats it
 }
 void loop()
 {
-  #if (_DEBUG > 0)
-	digitalWrite(LED_PIN, LOW);
-	delay(100);
-	digitalWrite(LED_PIN, HIGH);
-	delay(100);
-  #endif
-  
-  #if (_DEBUG == 2)
-  // count.loop is 3 bits, hence overflows at 7->0
-  if(++(count.loop) == 0)
-  {
-    wwvb_tx.debug_time();
-    Serial.println();
-  }
-  #endif
+   #if (_DEBUG > 0)
+   // debug outputs at the end of each 1minute frame
+   if(wwvb_tx.end_of_frame == true)
+   {
+      wwvb_tx.end_of_frame = false; // clear the EOF bit
+      #if (_DEBUG == 2)
+	  wwvb_tx.debug_time();
+      Serial.println();
+      #endif
+      
+      digitalWrite(LED_PIN, HIGH);
+      delay(100);
+      digitalWrite(LED_PIN, LOW);
+	}
+   #endif
 }
