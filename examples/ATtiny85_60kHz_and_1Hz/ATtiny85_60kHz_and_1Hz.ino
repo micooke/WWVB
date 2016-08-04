6032529b723aeb8d3bf8ba701baaec1e4778c581
Mark Cooke <mark@creativecookies.net>
2016-08-04 11:57:13 +0930

    ## ATtiny85_60kHz_and_1Hz.ino == carrier_60kHz.ino * renamed carrier_60kHz.ino and CarrierFreqCalc.xls * Added 1Hz 50% duty cycle modulation signal output on PB3 (carrier signal is output on PB4) * removed #ifdef for switching between OC1B and OC1A

--- examples/ATtiny85_60kHz_and_1Hz/ATtiny85_60kHz_and_1Hz.ino
+++ examples/ATtiny85_60kHz_and_1Hz/ATtiny85_60kHz_and_1Hz.ino
@@ -1,14 +1,16 @@
 //ATtiny85
-//                          +-\/-+
-//                 RST PB5 1|*   |8 VCC
-//                     PB3 2|    |7 PB2
-// 60kHz (USE_OC1B) <= PB4 3|    |6 PB1 => (USE_OC1A) 60kHz
-//                     GND 4|    |5 PB0
-//                          +----+
-
-#define USE_OC1B
+//                        +-\/-+
+//               RST PB5 1|*   |8 VCC
+// 1Hz modulation <= PB3 2|    |7 PB2
+// 60kHz carrier  <= PB4 3|    |6 PB1
+//                   GND 4|    |5 PB0
+//                        +----+
 
 volatile uint8_t isr_count = 0;
+#define DIVIDER_60kHz_TO_1Hz 30000
+volatile uint16_t _60kHz_to_1Hz = DIVIDER_60kHz_TO_1Hz;
+volatile bool mod_state = false;
+volatile uint8_t mod_pin = PB3;
 
 // Set PWM to 60kHz (8MHz / (132 + 1)) = 60150Hz
 // 132 resulted in 63492Hz
@@ -20,7 +22,8 @@ volatile uint8_t isr_count = 0;
 // 139 resulted in 60369Hz
 // 140 resulted in 59884Hz
 
-const uint8_t PWM_60kHz[2] = { 139, 140 };
+const uint8_t PWM_60kHz[2] = { 132, 133 };
+//const uint8_t PWM_60kHz[2] = { 139, 140 }; // Tuned
 
 ISR(TIMER1_OVF_vect)
 {
@@ -34,19 +37,19 @@ ISR(TIMER1_OVF_vect)
 	//
 	switch (isr_count)
 	{
-	/*
 	// First (failed) attempt
 	case 0:
 	case 1:
-		OCR1C = 132;
+		OCR1C = PWM_60kHz[0];
 		++isr_count;
 		break;
 	case 2:
-		OCR1C = 133;
+		OCR1C = PWM_60kHz[1];
 		isr_count = 0;
 		break;
-	*/
+	
 	// Second/Third/Forth attempt
+	/*
 	case 0:
 	case 1:
 	case 2:
@@ -63,11 +66,28 @@ ISR(TIMER1_OVF_vect)
 		OCR1C = PWM_60kHz[0];
 		isr_count = 0;
 		break;
+	*/
+	}
+
+	// Divide down the clock to 10Hz
+	switch (_60kHz_to_1Hz)
+	{
+	case 0:
+		digitalWrite(mod_pin, mod_state);
+		mod_state = !mod_state;
+		_60kHz_to_1Hz = DIVIDER_60kHz_TO_1Hz;
+		break;
+	default:
+		--_60kHz_to_1Hz;
+		break;
 	}
 }
 
 void setup()
 {
+	// set the modulation pin as output
+	pinMode(mod_pin, OUTPUT);
+
 	// Generate 60kHz carrier
 	// OC0A | !OC1A : 0 PB0 MOSI PWM 8b
 	// OC1A |  OC0B : 1 PB1 MISO PWM 8b
@@ -75,38 +95,31 @@ void setup()
 	//        !OC1B : 3 PB3
 	PLLCSR = _BV(PCKE) | _BV(PLLE); // enable 64MHz clock
 	
+	/// TIMER1
 	// Note : I tried using the internal 16MHz or 8MHz clock, but it didnt work
 	//        This is what is in the wwvb.h, hence wwvb.h doesnt work for attiny chips anymore :(
 	//        I assume that the arduino framework for the attinys stuffs this, likely due to the delay routine
 	//
-	TCCR1 = _BV(CS12); 		// Internal 64MHz PCK, prescalar = 8 (64MHz/8 = 8MHz)
-
+	// Note2 : Using the system clock changes the PWM mode, which uses 255 as the TOP value
+	//
 	OCR1C = PWM_60kHz[0]; 	// Set PWM to 60kHz (8MHz / (132 + 1)) = 60150Hz
+	TCCR1 = 0;
 
-#if defined(USE_OC1A)
-	pinMode(PB1, OUTPUT);	// setup OC1A PWM pin as output
-
-	TCCR1 |= _BV(PWM1A)  	// Clear timer/counter after compare match to OCR1C
-		| _BV(COM1A1); 		// Clear the OC1A output line after match
-
-	OCR1A = 66;				// ~50% duty cycle
-#elif defined(USE_OC1B)
 	pinMode(PB4, OUTPUT);	// setup OC1B PWM pin as output
 
 	GTCCR = _BV(PWM1B) 		// Clear timer/counter after compare match to OCR1C
 		| _BV(COM1B1);		// Clear the OC1B output line after match
 
 	OCR1B = 66;				// ~50% duty cycle
-#endif
 
 	// Enable Timer1 overflow interrupt
-	TIMSK = _BV(TOIE1);	// enable overflow interrupt on Timer1
+	TIMSK |= _BV(TOIE1);
 
 	// enable interrupts
 	sei();
 
 	// Start transmission (Set the clock prescalar)
-	TCCR1 |= _BV(CS12); // Set clock prescalar to 8 (64MHz / 8 = 8MHz)
+	TCCR1 |= _BV(CS12); // Set clock to PCK/8 (64MHz / 8 = 8MHz)
 }
 
 void loop() {}
